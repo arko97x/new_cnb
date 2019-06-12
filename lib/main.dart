a94a99159943d7b649edfb0caaabd4e20198954e
Arko <bhattacharjee.arkoprabho@gmail.com>
2019-06-12 22:11:29 +0530

    Added code to force PORTRAIT Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter_launcher_icons/android.dart';
-import 'package:flutter_launcher_icons/constants.dart';
-import 'package:flutter_launcher_icons/custom_exceptions.dart';
-import 'package:flutter_launcher_icons/ios.dart';
-import 'package:flutter_launcher_icons/main.dart';
-import 'package:flutter_launcher_icons/xml_templates.dart';
+import 'package:flutter/services.dart';
+// import 'package:flutter_launcher_icons/android.dart';
+// import 'package:flutter_launcher_icons/constants.dart';
+// import 'package:flutter_launcher_icons/custom_exceptions.dart';
+// import 'package:flutter_launcher_icons/ios.dart';
+// import 'package:flutter_launcher_icons/main.dart';
+// import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Roboto'),
       home: MyHomePage(),
